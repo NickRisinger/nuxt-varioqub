06c06ce9cd653ca371c160ded5536ee9bc37d057
NickRisinger <boozy044@gmail.com>
2025-01-13 00:59:31 +0300

    🔥 initial plugin project

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+export default createConfigForNuxt({
+  features: {
+    tooling: true,
+    typescript: true,
+    stylistic: {
+      indent: 'tab',
+      semi: true,
+    },
+  },
+})
